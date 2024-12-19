3e5cc1d7dec42e0baa9d77d4f60e781e1d11b660
guclo33 <guillaume_cloutier123@hotmail.com>
2024-12-19 09:36:58 -0500

    connect render db

--- model/database.js
+++ model/database.js
@@ -1,12 +1,17 @@
 const {Pool} = require("pg");
 require("dotenv").config()
 
-const pool = new Pool({
+const pool = new Pool(
+    process.env.DB_RENDER_URL
+    
+    
+    
+    /*{
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-});
+}*/);
 
 module.exports = pool

ee1a2b28cf311968e4dcd9ee362608ce04bb2c16
guclo33 <guillaume_cloutier123@hotmail.com>
2025-03-21 13:47:11 -0400

    ``` Aucune modification de code à enregistrer ```

--- server/canvaTemplate.js
+++ server/canvaTemplate.js
@@ -274,26 +274,42 @@ const connectCanvaDetail = async (req,res, next) => {
 }*/
 
 
-const generateTemplate =  (req, res) => {
-    const jsonData = req.body;
+const generateTemplate = async (req, res) => {
     const { clientid } = req.params;
     console.log("clientid", clientid);
 
-    
+    try {
         exec(`python ./GenerateurTexte/canvaAutofillExecute.py ${clientid}`, (error, stdout, stderr) => {
             if (error) {
-                // Si une erreur d'exécution se produit, on renvoie un message d'erreur
                 console.error("Erreur d'exécution:", error);
-                return res.status(500).send("Erreur lors de la génération du template");
+                return res.status(500).json({ message: "Erreur d'exécution du script Python" });
             }
-            
-            console.log("Résultat stdout :", stdout); // Affichage de la sortie du script Python
-            console.error("Messages stderr :", stderr); // Affichage des erreurs, si présentes
 
-            // Si tout s'est bien passé, renvoyer un message de succès
-            res.status(200).send("Template généré");
+            
+
+            // Utiliser une expression régulière pour extraire l'URL du tuple
+            const match = stdout.match(/(https:\/\/www\.canva\.com\/api\/design\/[^\s]+)/);
+            
+
+            if (match) {
+                const editUrl = match[0]; // L'URL est capturée dans le premier groupe de l'expression régulière
+                console.log("URL générée:", editUrl);
+
+                if (stderr) {
+                    console.error("Messages stderr :", stderr);
+                }
+
+                // Renvoyer l'URL au client sous forme de JSON
+                return res.status(200).json({ message: "Template généré avec succès", editUrl: editUrl });
+            } else {
+                console.error("Impossible de trouver l'URL dans la sortie");
+                return res.status(500).json({ message: "Erreur de parsing des données" });
+            }
         });
-    
+    } catch (error) {
+        console.log("Erreur lors de la génération du template:", error);
+        return res.status(500).json({ message: "Erreur lors de la génération du template", error });
+    }
 };
 
 

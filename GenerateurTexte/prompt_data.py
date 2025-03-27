dd46b229708690e24eef54e871e624b2c4ab09f4
guclo33 <guillaume_cloutier123@hotmail.com>
2025-03-27 11:56:36 -0400

    Add prompt retrieval and text generation functionality

--- GenerateurTexte/prompt_data.py
+++ GenerateurTexte/prompt_data.py
@@ -29,3 +29,31 @@ def get_prompt_data (selectedSetId, promptName) :
         print("Aucune valeur trouvé pour cet ID et nom de prompt.")
         return None
     
+
+def get_prompts (selectedSetId) :
+    conn = psycopg2.connect(
+    dbname= os.getenv("DB_RENDER_DATABASE"),
+    user= os.getenv("DB_RENDER_USER"),
+    password= os.getenv("DB_RENDER_PASSWORD"),
+    host= os.getenv("DB_RENDER_HOST")
+)
+    cursor = conn.cursor()
+    
+    
+    cursor.execute("SELECT prompt_name, prompt_number, value FROM prompts WHERE prompt_set_id = %s", [selectedSetId])
+    prompts = cursor.fetchall() 
+    
+
+     
+    
+    cursor.close()
+    conn.close()
+    
+    if prompts:
+         
+        return prompts 
+    else:
+        print("Aucune valeur trouvé pour cet ID de prompt.")
+        return None
+    
+
